876f3e01011eefba114ff7a3726c7d5b69f6131e
Riddhesh Dobariya <139103343+Riddheshdobariya@users.noreply.github.com>
2024-02-05 21:03:20 +0530

    Add files via upload

---
+++ JS/Homework(03_02_2024).js
@@ -0,0 +1,23 @@
+//*******************************-----------------------**************************************/
+function funObj({ name, age, city, address }) {
+    console.log("my name is ", name);
+    console.log("my age is ", age);
+    console.log("my city is ", city);
+    console.log("my address is ", address);
+}
+
+funObj({ name: "Riddhesh", age: 27, city: "Surat", address: `Varachha Surat,Pin:395006` });
+
+
+//*******************************-----------------------**************************************/
+
+function funArr([a, c, b]) {
+    console.log("-----------  a----------->", a);
+    console.log("-----------  c----------->", c);
+    console.log("-----------  b----------->", b);
+}
+
+funArr([100, 200, 900]);
+
+
+//*******************************-----------------------**************************************/
